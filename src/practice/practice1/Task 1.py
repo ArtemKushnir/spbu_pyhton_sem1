bdecf73d7d0cbdd93fa35bd8985d977efeb2efb1
ArtemKushnir <artem.kushnir.79@gmail.com>
2023-09-13 19:44:57 +0300

    add practice from first lection

---
+++ src/practice/practice1/Task 1.py
@@ -0,0 +1,15 @@
+def division(a, b):
+    k = 0
+    while a > b:
+        k += 1
+        a -= b
+    return k
+
+
+if __name__ == "__main__":
+    print('Неполное частное от деления a на b')
+    print('Введите число a')
+    a = int(input())
+    print('Введите число b')
+    b = int(input())
+    print(f'Неполное частное от деления a на b: {division(a, b)}, остаток: {a - b*division(a, b)}')
