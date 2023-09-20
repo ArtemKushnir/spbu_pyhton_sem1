0299f02536874979844060c59bbc7bc59c5c222e
Artem Kushnir <artem.kushnir.79@gmail.com>
2023-09-21 00:16:51 +0300

    checking for string validity

--- src/Homeworks/Homework2/DNA.py
+++ src/Homeworks/Homework2/DNA.py
@@ -5,19 +5,22 @@ def read(file_read, file_record):
     with open(file_read) as f_read:
         m = int(f_read.readline())
         dna = f_read.readline()
-        n = int(f_read.readline())
-        for i in range(n):
-            line_command = f_read.readline().split()
-            if line_command[0] == "DELETE":
-                dna = delete(dna, line_command[1:])
-                record(dna, file_record)
-            elif line_command[0] == "INSERT":
-                dna = insert(dna, line_command[1:])
-                record(dna, file_record)
-            elif line_command[0] == "REPLACE":
-                dna = replace(dna, line_command[1:])
-                record(dna, file_record)
-
+        if re.compile("[ACGT]").search(dna) is None:
+            return None
+        else:
+            n = int(f_read.readline())
+            for i in range(n):
+                line_command = f_read.readline().split()
+                if line_command[0] == "DELETE":
+                    dna = delete(dna, line_command[1:])
+                    record(dna, file_record)
+                elif line_command[0] == "INSERT":
+                    dna = insert(dna, line_command[1:])
+                    record(dna, file_record)
+                elif line_command[0] == "REPLACE":
+                    dna = replace(dna, line_command[1:])
+                    record(dna, file_record)
+    return True
 
 def record(dna, file_record):
     with open(file_record, "a") as f_record:
@@ -53,4 +56,7 @@ if __name__ == "__main__":
     file_name_read = input()
     print("Название файла для записи")
     file_name_record = input()
-    read(file_name_read, file_name_record)
+    if read(file_name_read, file_name_record) is None:
+        print("error")
+    else:
+        read(file_name_read, file_name_record)
