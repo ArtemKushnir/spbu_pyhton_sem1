4851d7aa7293d642ac24cf598f0134df03def9cc
Artem Kushnir <artem.kushnir.79@gmail.com>
2023-10-02 18:03:16 +0300

    improved readability

--- src/Homeworks/Homework2/DNA.py
+++ src/Homeworks/Homework2/DNA.py
@@ -24,28 +24,26 @@ def record(dna, file_record):
 
 
 def delete(dna, argument1, argument2):
-    return (
-        dna[: dna.find(argument1)]
-        + dna[
-            dna.find(argument2, dna.find(argument1) + len(argument1)) + len(argument2) :
-        ]
-    )
+    index_arg1 = dna.find(argument1)
+    first_part = dna[:index_arg1]
+    second_part = dna[
+        dna.find(argument2, index_arg1 + len(argument1)) + len(argument2) :
+    ]
+    return first_part + second_part
 
 
 def insert(dna, argument1, argument2):
-    return (
-        dna[: dna.find(argument1) + len(argument1)]
-        + argument2
-        + dna[dna.find(argument1) + len(argument1) :]
-    )
+    index_arg1 = dna.find(argument1)
+    first_part = dna[: index_arg1 + len(argument1)]
+    second_part = dna[index_arg1 + len(argument1) :]
+    return first_part + argument2 + second_part
 
 
 def replace(dna, argument1, argument2):
-    return (
-        dna[: dna.find(argument1)]
-        + argument2
-        + dna[dna.find(argument1) + len(argument1) :]
-    )
+    index_arg1 = dna.find(argument1)
+    first_part = dna[:index_arg1]
+    second_part = dna[index_arg1 + len(argument1) :]
+    return first_part + argument2 + second_part
 
 
 if __name__ == "__main__":
