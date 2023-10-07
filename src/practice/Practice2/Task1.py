b66652a93055451d17a6aa6148326bafb66bde3b
Artem Kushnir <artem.kushnir.79@gmail.com>
2023-10-08 01:13:31 +0300

    optimization of the solution, correction output

--- src/practice/Practice2/Task1.py
+++ src/practice/Practice2/Task1.py
@@ -1,23 +1,17 @@
 def fractions(n):
-    res = []
-    for n1 in range(2, n + 1):
-        for n2 in range(1, n1):
-            if common_divisors(n1, n2):
-                res.append(f"{n2}/{n1}")
-    return res
-
-
-def common_divisors(n1, n2):
-    for i in range(2, min(n1, n2) + 1):
-        if n1 % i == 0 and n2 % i == 0:
-            return False
-    return True
+    res = {}
+    for n1 in range(1, n):
+        for n2 in range(n1 + 1, n + 1):
+            if n1 / n2 not in res.values():
+                res[f"{n1}/{n2}"] = n1 / n2
+    return dict(sorted(res.items(), key=lambda item: item[1]))
 
 
 if __name__ == "__main__":
     print("Введите число n")
     n = int(input())
     if n <= 1:
-        print("Ошибка")
+        print("Ошибка, знаменатель должен быть больше 1")
     else:
-        print(f"Все подходящие дроби:", ", ".join(fractions(n)))
+        print("Все подходящие дроби:")
+        print(", ".join(fractions(n).keys()))
