ef321bdc7dbb202d985d763feee2b210a5c6d46a
Artem Kushnir <artem.kushnir.79@gmail.com>
2023-10-12 20:19:24 +0300

    add output function, change storing fractions

--- src/practice/Practice2/Task1.py
+++ src/practice/Practice2/Task1.py
@@ -1,17 +1,27 @@
-def fractions(n):
-    res = {}
-    for n1 in range(1, n):
-        for n2 in range(n1 + 1, n + 1):
-            if n1 / n2 not in res.values():
-                res[f"{n1}/{n2}"] = n1 / n2
-    return dict(sorted(res.items(), key=lambda item: item[1]))
+def find_fractions(max_denominator):
+    res = []
+    for numerator in range(1, max_denominator):
+        for denominator in range(numerator + 1, max_denominator + 1):
+            if numerator == 1:
+                res.append((numerator, denominator))
+            elif denominator % numerator != 0:
+                res.append((numerator, denominator))
+    res.sort(key=lambda x: x[0] / x[1])
+    return res
+
+
+def output_fractions(numerators_and_denominators):
+    print("Все подходящие дроби:")
+    fractions = map(lambda fraction: f"{fraction[0]}/{fraction[1]}", numerators_and_denominators)
+    return ", ".join(fractions)
 
 
 if __name__ == "__main__":
-    print("Введите число n")
+    print("Введите знаменатель:")
     n = int(input())
     if n <= 1:
         print("Ошибка, знаменатель должен быть больше 1")
     else:
-        print("Все подходящие дроби:")
-        print(", ".join(fractions(n).keys()))
+        all_fractions = find_fractions(n)
+        print(output_fractions(all_fractions))
+        
